c37f2e876f281cfb410fb390bcfe701cd13200fa
moskvoretskijviktor <enemy1307@mail.ru>
2023-04-24 17:09:54 +0300

    colab support + codestyle

--- evaluation/data_collat.py
+++ evaluation/data_collat.py
@@ -6,16 +6,21 @@ class DataCollatorWithPadding:
     def __init__(self, tokenizer):
         self.tokenizer = tokenizer
 
-    def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
+    def __call__(
+        self, features: List[Dict[str, Union[List[int], torch.Tensor]]]
+    ) -> Dict[str, torch.Tensor]:
         batch = self.tokenizer.pad(
             features,
             padding=True,
         )
         ybatch = self.tokenizer.pad(
-            {'input_ids': batch['labels'], 'attention_mask': batch['decoder_attention_mask']},
+            {
+                "input_ids": batch["labels"],
+                "attention_mask": batch["decoder_attention_mask"],
+            },
             padding=True,
-        ) 
-        batch['labels'] = ybatch['input_ids']
-        batch['decoder_attention_mask'] = ybatch['attention_mask']
-        
+        )
+        batch["labels"] = ybatch["input_ids"]
+        batch["decoder_attention_mask"] = ybatch["attention_mask"]
+
         return {k: torch.tensor(v) for k, v in batch.items()}
